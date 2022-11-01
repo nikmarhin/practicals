92df12db5e203bef7faba9f99bde17e2e62202fb
nikmarhin <89563125+nikmarhin@users.noreply.github.com>
2022-11-01 21:16:49 +1000

    Edited further

--- prac_02/score.py
+++ prac_02/score.py
@@ -10,13 +10,13 @@ def main():
     score = float(input("Enter score: "))
     get_result(score)
     print(result)
-    print(random.randint(0,100))
+    print(random.randint(0, 100))
 
 
 def get_result(score):
-    if score < 0 or score > 100:
+    result = ""
+    while score < 0 or score > 100:
         result = "Invalid score"
-    else:
         if score >= 90:
             result = "Excellent"
         elif score >= 50:
