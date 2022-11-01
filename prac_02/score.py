83b63f365dc30774fe068fb23edac719398a1955
nikmarhin <89563125+nikmarhin@users.noreply.github.com>
2022-11-01 21:12:33 +1000

    Added function, plus 'random' code

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,16 +1,29 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404 - Practical 2
 """
 
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
+
+def main():
+    result = ""
+    score = float(input("Enter score: "))
+    get_result(score)
+    print(result)
+    print(random.randint(0,100))
+
+
+def get_result(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
     else:
-        print("Bad")
+        if score >= 90:
+            result = "Excellent"
+        elif score >= 50:
+            result = "Passable"
+        else:
+            result = "Bad"
+    return result
+
+
+main()
